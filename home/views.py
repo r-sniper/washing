ad254150b8c5dfac4f22c9e64b8cc91d465d1ce3
akzarma <akzarma@gmail.com>
2019-05-09 04:41:09 +0530

    Merge branch 'master' of https://github.com/r-sniper/washing
    # Conflicts:
    #	db.sqlite3

--- home/views.py
+++ home/views.py
@@ -23,7 +23,7 @@ from washing.settings import MEDIA_ROOT
 def order_to_dict(order):
     return {'name': order.customer.name, 'mobile': order.customer.mobile,
             'customer_pk': order.customer.pk,
-            'received_date': str(order.received_date), 'delivery_date': str(order.delivery_date),
+            'received_date': str(order.received_date.strftime('%Y-%m-%d %H:%M')), 'delivery_date': str(order.delivery_date.strftime('%Y-%m-%d %H:%M')),
             'order_pk': order.pk, 'price': str(order.price), 'kg': str(order.kg), 'status': order.status,
             'address': order.customer.address}
 
