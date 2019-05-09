37fc5d47d10d8c560ac74c33bc7d86f68e0df366
akzarma <akzarma@gmail.com>
2019-05-09 06:03:34 +0530

    email send

--- home/models.py
+++ home/models.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from home.email_sender import send_email
+
+
 # Create your models here.
-from django.db.models.signals import pre_save, post_save
-from django.dispatch import receiver
 
 
 class UserDetails(models.Model):
@@ -31,7 +32,37 @@ class Order(models.Model):
 
     def save(self, *args, **kwargs):
         super(Order, self).save(*args, **kwargs)
+        subject = ''
+        msg = ''
+        if self.status == 1:
+            subject = 'Order Received'
+            msg = '<b>Order Details:</b><br>' \
+                  'Total Weight: ' + str(self.kg) + '<br>Price: ' + str(self.price)
+        elif self.status == 2:
+            subject = 'Order Details'
+            msg = '<b>Order Details:</b><br>' \
+                  'Order No: ' + str(self.pk) +'<br>Total Weight: ' + str(self.kg) + ' Kg<br>Price: ' + str(self.price) \
+                  + '<br><b>Clothes Type</b><br>'
+            tmp = ''
+            for each in self.orderdetail_set.all():
+                if each.count >0:
+                    tmp += each.category.name + ': ' + str(each.count) + '<br>'
+            msg += tmp
+        elif self.status == 3:
+            subject = 'Order Pick-up Reminder'
+            msg = 'Order Details<br>' \
+                  'Order No: ' + str(self.pk) +'<br>Total Weight: ' + str(self.kg) + ' Kg<br>Price: ' + str(self.price) + '<br>Order No: ' + str(self.pk) \
+                  + '<br><br>Your order has been successfully washed. Kindly collect your clothes from Wool World.<br>' \
+                  + '<br>Hope to be at your service again.'
+        elif self.status == 4:
+            subject = 'Order Delivered'
+            msg = 'Order Details<br>' \
+                  'Order No: ' + str(self.pk)+'<br>Total Weight: ' + str(self.kg) + ' Kg<br>Price: ' + str(self.price) \
+                  + '<br>Your order has been successfully delivered on <br>' + (
+                      self.delivery_date.strftime('%d %m %y, %I %M %p')) \
+                  + '<br><br>Hope to be at your service again.'
 
+        send_email('akzarma@gmail.com', subject, msg)
         price = self.price
         date = self.delivery_date
         if date:
